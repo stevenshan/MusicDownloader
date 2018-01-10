1b96accee57c6c56faf544af415b3993a0ef69a1
NickMohan <nickmohan12@gmail.com>
2018-01-10 01:45:14 -0500

    Youtube API Auth

--- MusicProject.py
+++ MusicProject.py
@@ -3,9 +3,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
+trackList = []
+
 #User inputs for album and artists
-#artist = input("Artist: ")
-artist = "queen"
 album = input("Album: ")
 
 #token authorization
@@ -13,7 +13,6 @@ client_id = "56fd49ba85c441919d120bae8c1cb7c5"
 client_secret = "78cf8ba2a5e545a3b4a8ff1c0a89f3da"
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-sp.trace = False
 
 #Album search to get the tracks on the albums
 results = sp.search(q = "album:" + album, type = "album")
@@ -23,9 +22,8 @@ for x in tracks['items']:
     print(x['name'])
 
 #Search the album dictionary for the artist uri
+artistURI = results['albums']['items'][0]['artists'][0]['uri']
+artist = results['albums']['items'][0]['artists'][0]['name']
 
 
-
-
-#The dictionary for albums has the artist uri. It can be found right after the
-#album uri almost. Get it from there.
+#Get youtube links for each song
---
+++ quickstart.py
@@ -0,0 +1,38 @@
+
+import os
+
+import google.oauth2.credentials
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+CLIENT_SECRETS_FILE = "client_secret_120449763734-ialbrmohqdfgtk6afb6epqma910tlvev.apps.googleusercontent.com.json"
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+def get_authenticated_service():
+  flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+  credentials = flow.run_console()
+  return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+
+def channels_list_by_username(service, **kwargs):
+  results = service.channels().list(
+    **kwargs
+  ).execute()
+
+  print('This channel\'s ID is %s. Its title is %s, and it has %s views.' %
+       (results['items'][0]['id'],
+        results['items'][0]['snippet']['title'],
+        results['items'][0]['statistics']['viewCount']))
+
+if __name__ == '__main__':
+  # When running locally, disable OAuthlib's HTTPs verification. When
+  # running in production *do not* leave this option enabled.
+  os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+  service = get_authenticated_service()
+  channels_list_by_username(service,
+      part='snippet,contentDetails,statistics',
+      forUsername='GoogleDevelopers')
